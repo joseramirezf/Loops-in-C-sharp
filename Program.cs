29bd8a574cbaac0b9521b90908286b2f1d3c1083
Jose Ramirez <joseramirez@Joses-MacBook-Pro.local>
2022-06-16 07:33:00 -0600

    Loops in C#

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("C# Loops");
+Console.WriteLine("Printing even numbers between two numbers\n");
+
+Console.Write("Enter the first number:");
+int firstNumber = Int32.Parse (Console.ReadLine() ?? "0");
+
+Console.Write("Enter the second number:");
+int secondNumber = Int32.Parse (Console.ReadLine() ?? "1000");
+
+for (int i = firstNumber; i <= secondNumber; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i);
+    }
+}
+
+Console.WriteLine("End of program");
